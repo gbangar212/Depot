da06ff42e7de3cfa1f60774ecd3f8141f3fbce1b
Gaurav Bangar <gbangar212@gmail.com>
2015-05-30 08:20:11 +0530

    caching

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,11 @@ class Product < ActiveRecord::Base
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   #validates title: uniqueness:  true
   validates :image_url, allow_blank: true, format: { with: %r{\.(gif|jpg|png)\Z}i, message: 'file must be type GIF, JPG or PNG'}
+
+def self.latest
+  Product.order(:updated_at).last
+end
+
+
+
 end
